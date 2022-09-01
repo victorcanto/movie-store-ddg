82aa7846ea4549140e2ed17bb6e79b85b0aac58e
Victor Canto <iamvictorcanto@gmail.com>
2022-09-01 13:29:51 -0300

    chore: project setup

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+	render(<App />);
+	const element = screen.getByText(/This is App/i);
+	expect(element).toBeInTheDocument();
 });
